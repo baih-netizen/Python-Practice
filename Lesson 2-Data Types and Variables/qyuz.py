ed28f33746e6fac95b7bb932561783fa011cc8c4
baih-netizen <baih@wsc.school.nz>
2026-05-19 15:08:15 +1200

    Update qyuz.py

---
+++ Lesson 2-Data Types and Variables/qyuz.py
@@ -0,0 +1,6 @@
+city = input("What is New Zealand's largest city?")
+if city == "Auckland":
+    print("Good job!")
+else:
+    print("It's wrong, please try again.")
+food = input("What is your favourite food?")
