462a766ecb7b998b906d95531f7d840c27260779
baih-netizen <baih@wsc.school.nz>
2026-06-23 13:59:29 +1200

    Update _main.py

--- _main.py
+++ _main.py
@@ -7,11 +7,22 @@ print(5 == '5')
 print(5 + 5 >= 10)
 print(5 + 5 >= 8)
 """
-
+""""
 guess = input('What’s the password?')
 print('Checking password is a match…')
 while guess != 'secret':
     guess = input('Try again')
     print('Checking password is a match…')
 input('Welcome!')
+"""
+# def calculate_area(x, y):
+#     print(f"Area : {x * y}")
+
+# calculate_area()
+
+def repeat_message(message, times):
+    for i in range(times):
+        print(message)
+
+repeat_message("Hello", e5)
 
